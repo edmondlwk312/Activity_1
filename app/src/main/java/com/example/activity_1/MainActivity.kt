5e15c22fd0ba9ce26b004cf728b37c7ad26c96c6
edmond <laiwk-wm17@student.tarc.edu.my>
2019-12-04 18:37:10 +0800

    Activity_1

--- app/src/main/java/com/example/activity_1/MainActivity.kt
+++ app/src/main/java/com/example/activity_1/MainActivity.kt
@@ -2,68 +2,69 @@ package com.example.activity_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity()
+{
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonCalculate.setOnClickListener()
-        {
-            val val1:String = editTextCarPrice.text.toString()
-            val val2:String = editTextDownPayment.text.toString()
-//            val val3:String = editTextInterestRate.text.toString()
-//            val val4:String = editTextLoanPeriod.text.toString()
+        val editCarPrice = findViewById(R.id.editTextCarPrice) as EditText
+        val editDownPayment = findViewById(R.id.editTextDownPayment) as EditText
+        val editLoanPeriod = findViewById(R.id.editTextLoanPeriod) as EditText
+        val editInterestRate = findViewById(R.id.editTextInterestRate) as EditText
 
-            val result1:Int = carLoan(val1, val2)
-            textViewLoan.setText(result1)
+        val viewLoan = findViewById(R.id.textViewLoan) as TextView
+        val viewInterest = findViewById(R.id.textViewInterest) as TextView
+        val viewMonthlyRepayment = findViewById(R.id.textViewMonthlyRepayment) as TextView
 
-//            val result2:Int = interest(val1, val2, val3, val4)
-//            textViewInterest.setText(result2)
-//
-//            val result3:Int = monthlyRepayment(val1, val2, val3, val4)
-//            textViewMonthlyRepayment.setText(result3)
-//            interest()
-        }
+        val buttonForCalculate = findViewById(R.id.buttonCalculate) as Button
+        val buttonForReset = findViewById(R.id.buttonReset) as Button
 
-//        buttonReset.setOnClickListener()
-//        {
-//
-//        }
+        buttonForCalculate.setOnClickListener(View.OnClickListener {
+            View -> viewLoan.text = "Loan : " + (carPrice() - downPayment()).toString();
+                    viewInterest.text = "Interest : " + ((carPrice() - downPayment()) * interestRate() * loanPeriod()).toString();
+                    viewMonthlyRepayment.text = "Monthly Repayment : " + ((((carPrice() - downPayment()) + ((carPrice() - downPayment()) * interestRate() * loanPeriod())) / loanPeriod()) / 12).toString()
+            }
+        )
+
+        buttonForReset.setOnClickListener(View.OnClickListener {
+            View -> editCarPrice.getText().clear();
+                    editDownPayment.getText().clear();
+                    editLoanPeriod.getText().clear();
+                    editInterestRate.getText().clear();
+                    viewLoan.text = "Loan : ";
+                    viewInterest.text = "Interest : ";
+                    viewMonthlyRepayment.text = "Monthly Repayment : "
+        })
     }
 
-    private fun carLoan(val1:String, val2:String): Int
+    fun carPrice(): Int
     {
-        val a:Int = Integer.parseInt(val1)
-        val b:Int = Integer.parseInt(val2)
-
-        //val sub:Int = a-b
-        return a-b
+        val input_CarPrice = findViewById(R.id.editTextCarPrice) as EditText
+        return Integer.parseInt(input_CarPrice.text.toString())
     }
 
-//    private fun interest(val1:String, val2:String, val3:String, val4:String): Int
-//    {
-//        val a:Int = Integer.parseInt(val1)
-//        val b:Int = Integer.parseInt(val2)
-//        val c:Int = Integer.parseInt(val3)
-//        val d:Int = Integer.parseInt(val4)
-//
-//        val sub:Int = (a-b)*c*d
-//        return sub
-//    }
+    fun downPayment(): Int
+    {
+        val input_DownPayment = findViewById(R.id.editTextDownPayment) as EditText
+        return Integer.parseInt(input_DownPayment.text.toString())
+    }
 
-//    private fun monthlyRepayment(val1:String, val2:String, val3:String, val4:String): Int
-//    {
-//        val a:Int = Integer.parseInt(val1)
-//        val b:Int = Integer.parseInt(val2)
-//        val c:Int = Integer.parseInt(val3)
-//        val d:Int = Integer.parseInt(val4)
-//
-//        val sub:Int = ((a-b)+((a-b)*c*d))/d/12
-//        return sub
-//    }
+    fun interestRate(): Int
+    {
+        val input_InterestRate = findViewById(R.id.editTextInterestRate) as EditText
+        return Integer.parseInt(input_InterestRate.text.toString())
+    }
+
+    fun loanPeriod(): Int
+    {
+        val input_LoadPeriod = findViewById(R.id.editTextLoanPeriod) as EditText
+        return Integer.parseInt(input_LoadPeriod.text.toString())
+    }
 }
-
